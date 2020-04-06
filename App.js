5f3caf90dae9b42b7f58eeb27f8664a4009b9abb
nelsonjr86 <nelsonjr86@gmail.com>
2020-04-06 12:27:08 -0300

    Redux Persist

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/es/integration/react';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { Provider } from 'react-redux';
-import Store from './src1/Store';
+import { store, persistor } from './src1/Store';
 import MainNavigator from './src1/navigators/MainNavigator';
 
 const AppContainer = createAppContainer(MainNavigator);
 
 export default () => (
-    <Provider store={Store}>
-        <AppContainer />
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor} >
+            <AppContainer />
+        </PersistGate>        
     </Provider>
 )
--- src1/Store.js
+++ src1/Store.js
@@ -1,4 +1,17 @@
 import { createStore } from "redux";
+import AsyncStorage from '@react-native-community/async-storage';
+import { persistStore, persistReducer } from 'redux-persist';
+
 import Reducers from './reducers/index';
-const store = createStore (Reducers);
-export default store;
+
+const persistedReducer = persistReducer({
+    key:'root',
+    storage:AsyncStorage,
+    whitelist:['userReducer'],
+    //blacklist:[]
+}, Reducers);
+
+const store = createStore (persistedReducer);
+let persistor = persistStore(store);
+
+export { store, persistor };
